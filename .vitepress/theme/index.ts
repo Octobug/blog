08754ae02f662990af7cdcb67cbb8c452d6d8751
Octobug <whalevocal@gmail.com>
2023-09-24 13:36:45 +0800

    dev: introduce typescript & eslint #1

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,17 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import Theme from "vitepress/theme";
+import "./style.css";
 
 export default {
   extends: Theme,
-  Layout: () => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Layout() {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
-}
+  },
+};

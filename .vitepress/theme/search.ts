693f528bb0ec5e228acd54e1f50b07981ef615a2
Octobug <whalevocal@gmail.com>
2023-11-04 13:13:23 +0800

    fix(search): work around with Intl.Segmenter on Firefox

--- .vitepress/theme/search.ts
+++ .vitepress/theme/search.ts
@@ -1,4 +1,9 @@
 export function tokenize(text: string): Array<string> {
+  // Firefox doesn't support Intl.Segmenter currently
+  if (!("Segmenter" in Intl)) {
+    return text.split(" ");
+  }
+
   // @ts-ignore: seems like Intl.Segmenter is not supported by the lang server
   const segmenter = new Intl.Segmenter("cn", { granularity: "word" });
   const words = Array.from(segmenter.segment(text)).map(

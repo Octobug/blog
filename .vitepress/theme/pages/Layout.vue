cac24fe1cf13cb903c2b45267bf2100f006cbf9f
Octobug <whalevocal@gmail.com>
2024-02-09 20:28:47 +0800

    refactor(css): move doc-specific css into Layout.vue

--- .vitepress/theme/pages/Layout.vue
+++ .vitepress/theme/pages/Layout.vue
@@ -19,5 +19,85 @@ import Comments from "../components/Comments.vue";
 const { Layout } = DefaultTheme;
 </script>
 
-<style scoped>
+<style>
+/* Post Item */
+.vp-doc h1 {
+  font-size: 25px;
+}
+
+.vp-doc h2 {
+  font-size: 22px;
+  margin-top: 40px;
+  margin-bottom: 20px;
+}
+
+.vp-doc h3 {
+  font-size: 19px;
+  margin-top: 24px;
+  margin-bottom: 12px;
+}
+
+.vp-doc h4 {
+  font-size: 17px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+/* hack for post elements's margin-bottom */
+.main div[name="post-elements"]+* {
+  margin-top: 0;
+}
+
+/* cover size */
+.main div[name="post-elements"]+figure img {
+  width: 100%;
+}
+
+.main img {
+  border-radius: 0.5rem;
+  box-shadow: 0 0.5rem 0.8rem var(--ct-c-shadow);
+  margin-bottom: 2rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.dark .main img {
+  border: 0.1rem solid var(--vp-c-neutral);
+  box-shadow: none;
+}
+
+.zoom07 {
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+}
+
+/* image subtitle */
+figure figcaption {
+  margin-top: -1.3rem;
+  margin-bottom: 1.5rem;
+  text-align: center;
+  color: var(--vp-c-text-3);
+  font-size: 13px;
+  font-weight: 400;
+}
+
+figure figcaption a {
+  color: var(--vp-c-text-3) !important;
+}
+
+/* Markdown Footnotes */
+sup.footnote-ref a {
+  text-decoration: none;
+}
+
+ol.footnotes-list li p {
+  margin: 0;
+  line-height: 24px;
+}
+
+ol.footnotes-list li p a.footnote-backref {
+  text-decoration: none;
+  font-family: monospace;
+}
 </style>

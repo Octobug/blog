87967504e3afe9ec795f37857259367256083f65
Shaobiao Lin <whalevocal@gmail.com>
2024-04-02 20:22:14 +0800

    fix(style): apply css to ref links in details blocks

--- .vitepress/theme/pages/Layout.vue
+++ .vitepress/theme/pages/Layout.vue
@@ -120,8 +120,8 @@ li.footnote-item p em {
   color: var(--vp-c-text-2);
 }
 
-sup.footnote-ref a {
-  color: var(--vp-c-text-3);
+sup.footnote-ref a[id^=fnref] {
+  color: var(--vp-c-text-2);
   font-size: 0.8rem;
 }
 </style>
